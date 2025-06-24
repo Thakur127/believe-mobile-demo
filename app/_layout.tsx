ff579cf86714ff22fb5f8c53c741eb3ca6744f03
Ravi Thakur <ravithakur.dev7@gmail.com>
2025-06-24 22:11:38 +0530

    theme setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,22 @@
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { useColorScheme } from "react-native";
 
 export default function RootLayout() {
-  return <Stack />;
+  const colorScheme = useColorScheme();
+
+  return (
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="not-found" options={{ title: "Oops!" }} />
+      </Stack>
+      <StatusBar style="auto" />
+    </ThemeProvider>
+  );
 }
